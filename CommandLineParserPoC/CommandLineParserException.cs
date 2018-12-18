49f0e3c4639c40c61aa64e536672e1fb2d8556f1
Andrew Savinykh <27190911+andrewsav-datacom@users.noreply.github.com>
2018-12-18 14:57:16 +1300

    another missing file ;)

---
+++ CommandLineParserPoC/CommandLineParserException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// ReSharper disable UnusedMember.Global
+
+namespace CommandLineParserPoC
+{
+    public class CommandLineParserException : Exception
+    {
+        public CommandLineParserException()
+        {
+        }
+
+        public CommandLineParserException(string message) : base(message)
+        {
+        }
+
+        public CommandLineParserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
